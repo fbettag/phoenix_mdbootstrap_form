792ad2ecfa975c8bd4424ddb120b55af9219e4d7
Franz Bettag <franz@bett.ag>
2019-11-08 13:21:37 +0100

    makes mix.exs use git tags for version.

--- mix.exs
+++ mix.exs
@@ -4,14 +4,15 @@ defmodule PhoenixMDBootstrapForm.Mixfile do
   @project_url "https://github.com/fbettag/phoenix_mdbootstrap_form"
 
   def project do
+	{tag, description} = git_version()
     [
       app:              :phoenix_mdbootstrap_form,
-      version:          "0.0.6",
+      version:          tag,
       elixir:           "~> 1.4",
       source_url:       @project_url,
       homepage_url:     @project_url,
       name:             "Phoenix Material Design Bootstrap Form",
-      description:      "Material Design Bootstrap 4 Forms for Phoenix Applications",
+      description:      "Material Design Bootstrap 4 Forms for Phoenix Applications " <> description,
       build_embedded:   Mix.env == :prod,
       start_permanent:  Mix.env == :prod,
       package:          package(),
@@ -39,5 +40,28 @@ defmodule PhoenixMDBootstrapForm.Mixfile do
       licenses:     ["MIT"],
       links:        %{"GitHub" => @project_url}
     ]
+  end
+
+  defp git_version() do
+    # pulls version information from "nearest" git tag or sha hash-ish
+    {hashish, 0} =
+      System.cmd("git", ~w[describe --dirty --abbrev=7 --tags --always --first-parent])
+
+    full_version = String.trim(hashish)
+
+    tag_version =
+      hashish
+      |> String.split("-")
+      |> List.first()
+      |> String.replace_prefix("v", "")
+      |> String.trim()
+
+    tag_version =
+      case Version.parse(tag_version) do
+        :error -> "0.0.0-#{tag_version}"
+        _ -> tag_version
+      end
+
+    {tag_version, full_version}
   end
 end
